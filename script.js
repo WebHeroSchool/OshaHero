62bd98bc71a6fd64e6d299f6e56a34032f3f80fa
ThinkpadT440\User <olgaspb1986@mail.ru>
2020-10-23 18:26:41 +0300

    Для проверки menu-icon

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const menuToggle = document.querySelector('#menuToggle');
+
+menuToggle.onclick = function(){
+  menuToggle.classList.toggle('menu-icon-active');
+}
+
+/*document.querySelector('.menu__icon').onclick = function(){
+ this.classList.toggle('menu__icon_active');
+}*/
